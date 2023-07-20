1936cdfda754191fd9df711622fff9c1ce5f97ce
Er-Carl Abban <102951384+KharlAbban@users.noreply.github.com>
2023-07-20 01:46:59 +0000

    Fixed location issue

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,85 +1,116 @@
-const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+const sideLinks = document.querySelectorAll(
+  ".sidebar .side-menu li a:not(.logout)"
+);
 const dateTimeCont = document.querySelector("[data-jumbotron-dateTime]");
 const weatherCont = document.querySelector("[data-jumbotron-weather]");
-const menuBar = document.querySelector('.content nav .bx.bx-menu');
-const sideBar = document.querySelector('.sidebar');
-const searchBtn = document.querySelector('.content nav form .form-input button');
-const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
-const searchForm = document.querySelector('.content nav form');
-const toggler = document.getElementById('theme-toggle');
+const menuBar = document.querySelector(".content nav .bx.bx-menu");
+const sideBar = document.querySelector(".sidebar");
+const searchBtn = document.querySelector(
+  ".content nav form .form-input button"
+);
+const searchBtnIcon = document.querySelector(
+  ".content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector(".content nav form");
+const toggler = document.getElementById("theme-toggle");
 
 //Function to save itemto localStorage
-function saveToStore (key, content) {
-    localStorage.setItem(key, content);
+function saveToStore(key, content) {
+  localStorage.setItem(key, content);
 }
 //Function to set Jumbotron info
 function setJumbotronStuff(locWeatherQuoteJson) {
-    saveToStore("locWeatherQuote_Info", JSON.stringify(locWeatherQuoteJson));
-    console.log(locWeatherQuoteJson);
-    weatherCont.querySelector("[data-jumbotron-location]").textContent = `${locWeatherQuoteJson.city}, ${locWeatherQuoteJson.country_code}`;
-    weatherCont.querySelector("[data-jumbotron-weather]").textContent = `${locWeatherQuoteJson.mainWeatherInfo.temp}`;
-    weatherCont.querySelector("[data-jumbotron-feels]").textContent = `${locWeatherQuoteJson.mainWeatherInfo.feels_like}`;
-    document.querySelector("[data-jumbotron-quote]").textContent = `${locWeatherQuoteJson.quoteData[0].q}`;
-    document.querySelector("[data-jumbotron-author]").textContent = `- ${locWeatherQuoteJson.quoteData[0].a}`;
+  saveToStore("locWeatherQuote_Info", JSON.stringify(locWeatherQuoteJson));
+  weatherCont.querySelector(
+    "[data-jumbotron-location]"
+  ).textContent = `${locWeatherQuoteJson.city}, ${locWeatherQuoteJson.country_code}`;
+  weatherCont.querySelector(
+    "[data-jumbotron-weather]"
+  ).textContent = `${locWeatherQuoteJson.mainWeatherInfo.temp}`;
+  weatherCont.querySelector(
+    "[data-jumbotron-feels]"
+  ).textContent = `${locWeatherQuoteJson.mainWeatherInfo.feels_like}`;
+  document.querySelector(
+    "[data-jumbotron-quote]"
+  ).textContent = `${locWeatherQuoteJson.quoteData[0].q}`;
+  document.querySelector(
+    "[data-jumbotron-author]"
+  ).textContent = `- ${locWeatherQuoteJson.quoteData[0].a}`;
 }
-//Send fetch request on DOMContentLoaded
+//Call function on DOMContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
-    let weatherStuff;
-    fetch("/home").then(res => res.json()).then(data => {
-        setJumbotronStuff(data);
-    });
+  fetchStuff();
 });
+//Function to call on DOMContentLoaded
+async function fetchStuff() {
+  const response = await fetch("http://api.ipify.org/?format=json");
+  const ipResponse = await response.json();
+  await fetch("/home", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(ipResponse),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      setJumbotronStuff(data);
+    });
+}
 
 //Set date and time for current device
-dateTimeCont.querySelector("[data-jumbotron-date]").textContent = new Date().toDateString();
-function setTime () {
-    dateTimeCont.querySelector("[data-jumbotron-time]").textContent = new Date().toLocaleTimeString();
+dateTimeCont.querySelector("[data-jumbotron-date]").textContent =
+  new Date().toDateString();
+function setTime() {
+  dateTimeCont.querySelector("[data-jumbotron-time]").textContent =
+    new Date().toLocaleTimeString();
 }
 setInterval(setTime, 1000);
 //Add active class to sidebar links
-sideLinks.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', () => {
-        sideLinks.forEach(i => {
-            i.parentElement.classList.remove('active');
-        })
-        li.classList.add('active');
-    })
+sideLinks.forEach((item) => {
+  const li = item.parentElement;
+  item.addEventListener("click", () => {
+    sideLinks.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
 //Minify sidebar when menu icon is clicked
-menuBar.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
-    document.querySelector(".profile-bg-img").classList.toggle("mkSmall");
+menuBar.addEventListener("click", () => {
+  sideBar.classList.toggle("close");
+  document.querySelector(".profile-bg-img").classList.toggle("mkSmall");
 });
 //Add EventListener to search button
-searchBtn.addEventListener('click', function (e) {
-    if (window.innerWidth < 576) {
-        e.preventDefault;
-        searchForm.classList.toggle('show');
-        if (searchForm.classList.contains('show')) {
-            searchBtnIcon.classList.replace('bx-search', 'bx-x');
-        } else {
-            searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        }
+searchBtn.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault;
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchBtnIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchBtnIcon.classList.replace("bx-x", "bx-search");
     }
+  }
 });
 //Change element classes on window resize
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 768) {
-        sideBar.classList.add('close');
-    } else {
-        sideBar.classList.remove('close');
-    }
-    if (window.innerWidth > 576) {
-        searchBtnIcon.classList.replace('bx-x', 'bx-search');
-        searchForm.classList.remove('show');
-    }
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    sideBar.classList.add("close");
+  } else {
+    sideBar.classList.remove("close");
+  }
+  if (window.innerWidth > 576) {
+    searchBtnIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
 });
 //Event Listener for theme toggle click
-toggler.addEventListener('change', function () {
-    if (this.checked) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
+toggler.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
 });
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !=='production') {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
@@ -7,7 +7,6 @@ const app = express();
 const server_port = process.env.PORT;
 app.use(express.json());
 app.use(express.static("frontend"));
-const IPGEOLOC_URL = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEOLOC_API_KEY}`;
 const notesRouter = require("./routes/notes_route");
 const playgroundRouter = require("./routes/playground_route");
 const profileRouter = require("./routes/profile_route");
@@ -23,38 +22,43 @@ app.listen(server_port || "3000", () => {
   console.log("Server is running!");
 });
 
-app.get("/home", (req, res) => {
-  let locWeatherObject = {}
-  let ipLatitude, ipLongitude
+app.post("/home", (req, res) => {
+  const ipLocation = req.body.ip;
+  const IPGEOLOC_URL = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEOLOC_API_KEY}&ip=${ipLocation}`;
+  let locWeatherObject = {};
+  let ipLatitude, ipLongitude;
   let WEATHER_URL;
   //First call to ipgeolocation API
   axios({
     url: IPGEOLOC_URL,
-    responseType: 'json'
-  }).then(locData => {
-    locWeatherObject.city = locData.data.city;
-    locWeatherObject.country_code = locData.data.country_code2;
-    locWeatherObject.country_name = locData.data.country_name;
-    ipLatitude = locData.data.latitude;
-    ipLongitude = locData.data.longitude;
-    return locData;
-  }).then(locData => {
-    WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${ipLatitude}&lon=${ipLongitude}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
-    //Nested Axios call
-    axios({
-      url: WEATHER_URL,
-      responseType: 'json'
-    }).then(weatherData => {
-      locWeatherObject.mainWeatherInfo = weatherData.data.main;
-      locWeatherObject.weatherDesc = weatherData.data.weather;
+    responseType: "json",
+  })
+    .then((locData) => {
+      locWeatherObject.city = locData.data.city;
+      locWeatherObject.country_code = locData.data.country_code2;
+      locWeatherObject.country_name = locData.data.country_name;
+      ipLatitude = locData.data.latitude;
+      ipLongitude = locData.data.longitude;
+      return locData;
+    })
+    .then((locData) => {
+      WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${ipLatitude}&lon=${ipLongitude}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`;
+      //Nested Axios call
       axios({
-        url: "https://zenquotes.io/api/random",
-        responseType: "json"
-      }).then(quoteData => {
-        locWeatherObject.quoteData = quoteData.data;
-        res.json(locWeatherObject);
+        url: WEATHER_URL,
+        responseType: "json",
+      }).then((weatherData) => {
+        locWeatherObject.mainWeatherInfo = weatherData.data.main;
+        locWeatherObject.weatherDesc = weatherData.data.weather;
+        axios({
+          url: "https://zenquotes.io/api/random",
+          responseType: "json",
+        }).then((quoteData) => {
+          locWeatherObject.quoteData = quoteData.data;
+          res.json(locWeatherObject);
+        });
       });
-    });
-    //End of nested axios call
-  }).catch(err => console.log(err));
+      //End of nested axios call
+    })
+    .catch((err) => console.log(err));
 });
